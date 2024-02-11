de9f88ca16c6778d9319592dc913eb6ff58abc05
Tomasberro <tomasberrotaran@gmail.com>
2024-02-11 01:01:18 -0300

    app countries checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Country, countriesData } from './models/Country';
 
-function App() {
+
+const App: React.FC = () => {
+  const [countries, setCountries] = useState<Country[]>(countriesData);
+  const [selectAll, setSelectAll] = useState(false);
+
+  useEffect(() => {
+    const allSelected = countries.every(country => country.selected);
+    if (allSelected !== selectAll) {
+      setSelectAll(allSelected);
+    }
+  }, [countries, selectAll]);
+
+  const handleCheckboxChange = (index: number) => {
+    const updatedCountries = [...countries];
+    updatedCountries[index].selected = !updatedCountries[index].selected;
+    setCountries(updatedCountries);
+    setSelectAll(false);
+  };
+
+  const handleSelectAllChange = () => {
+    const updatedCountries = countries.map(country => ({
+      ...country,
+      selected: !selectAll
+    }));
+    setCountries(updatedCountries);
+    setSelectAll(!selectAll);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="checkbox-container">
+        <label>
+          <input type="checkbox" checked={selectAll} onChange={handleSelectAllChange} />
+          Select All
+        </label>
+     </div>
+      {countries.map((country, index) => (
+        <div  className="checkbox-container" key={index}>
+          <label>
+            <input
+              type="checkbox"
+              checked={country.selected}
+              onChange={() => handleCheckboxChange(index)}
+            />
+            {country.name}
+          </label>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
+
+
---
+++ src/models/Country.ts
@@ -0,0 +1,12 @@
+
+export interface Country {
+    name: string;
+    selected: boolean;
+  }
+  
+export const countriesData: Country[] = [
+    { name: 'India', selected: false },
+    { name: 'Usa', selected: false },
+    { name: 'France', selected: false },
+  ];
+  
